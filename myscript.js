c484fa0b975beba2fda4c154f879f6438358e3ba
Masadavide <davide.masadm@gmail.com>
2021-12-20 19:24:46 +0100

    quasi funzionante (sistemare grandezza box)

---
+++ myscript.js
@@ -0,0 +1,29 @@
+let btn = document.getElementById("btn-play");
+
+btn.addEventListener("click",function(){
+    let grid = document.getElementById('grid');
+    let level =  document.getElementById('level').value;
+    grid.innerHTML = "";
+    if(level == 1){
+        for(i = 1; i <= 100; i++){
+            grid.innerHTML += `<div class="box box1">${i}</div>`
+        }
+    }else if(level == 2){
+        for(i = 1; i <= 81; i++){
+            grid.innerHTML += `<div class="box box2">${i}</div>`
+        }
+    }else{
+        for(i = 1; i <= 49; i++){
+            grid.innerHTML += `<div class="box box3">${i}</div>`
+        }
+    }
+
+    let box = document.getElementsByClassName('box')
+    for (i = 0; i < box.length; i++)
+        box[i].addEventListener("click",function(){
+            let color = "";
+            this.classList.add("color");
+    })
+})
+
+
